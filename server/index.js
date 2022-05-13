9406ca747389b361561a73042e7c06e2bd748bae
Aayush <aayush5emails@gmail.com>
2022-05-13 19:38:28 +0530

    diirname

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import connection from "./DB/db.js";
 import Router from "./Routes/Routes.js";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import path from "path";
 
 const app = express();
 app.use(cors());
@@ -11,10 +12,17 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 dotenv.config();
-
-app.use("/", Router);
-
 const PORT = process.env.PORT || 8000;
+app.use("/", Router);
+app.use(express.json());
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("Server Is Running On Port 8000");
