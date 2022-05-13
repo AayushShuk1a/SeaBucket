6d172415a314d44f7230ea068acd2fa6bd8b326f
Aayush <aayush5emails@gmail.com>
2022-05-13 19:26:42 +0530

    hello

--- server/client/src/Components/API/ApiCalls.js
+++ server/client/src/Components/API/ApiCalls.js
@@ -3,19 +3,19 @@ import axios from "axios";
 const URL = "https://fishbucket.herokuapp.com/";
 
 export const getFaqs = async () => {
-  const FAQ = await axios.get(`${URL}faq`);
+  const FAQ = await axios.get(`${URL}/faq`);
 
   return FAQ.data;
 };
 
 export const getReads = async () => {
-  const read = await axios.get(`${URL}read`);
+  const read = await axios.get(`${URL}/read`);
 
   return read.data;
 };
 
 export const getTopics = async () => {
-  const topic = await axios.get(`${URL}topic`);
+  const topic = await axios.get(`${URL}/topic`);
 
   return topic.data;
 };
